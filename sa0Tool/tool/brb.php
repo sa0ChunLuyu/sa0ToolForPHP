e78b9c283cac7bcda4656e3e847acf047651e1fa
sa0ChunLuyu <sa0Chun_luyu@aliyun.com>
2019-11-22 11:34:54 +0800

    Update brb.php

--- sa0Tool/tool/brb.php
+++ sa0Tool/tool/brb.php
@@ -1,13 +1,16 @@
 <?php
-class baseRc4Base {
-    public static function setEncrypt($data,$rc4key=RC4KEY,$replace=false){
+
+class baseRc4Base
+{
+    public function setEncrypt($data, $rc4key = RC4KEY, $replace = false)
+    {
         if (is_array($data)) {
             $data = json_encode($data, JSON_UNESCAPED_UNICODE);
         }
-        $data=base64_encode($data);
-        $result=self::rc4($data,$rc4key);
-        $result=base64_encode($result);
-        if($replace){
+        $data = base64_encode($data);
+        $result = $this->rc4($data, $rc4key);
+        $result = base64_encode($result);
+        if ($replace) {
             $result = str_replace('=', '__', $result);
             $result = str_replace('+', '-', $result);
             $result = str_replace('/', '_', $result);
@@ -15,27 +18,28 @@ class baseRc4Base {
         return $result;
     }
 
-    public static function setDecrypt($data,$rc4key=RC4KEY,$replace=false){
+    public function setDecrypt($data, $rc4key = RC4KEY, $replace = false)
+    {
         if (!$data) {
             return false;
         }
-        $data=str_replace(" ","+",$data);
+        $data = str_replace(" ", "+", $data);
 
-        if($replace){
+        if ($replace) {
             $data = str_replace('__', '=', $data);
             $data = str_replace('-', '+', $data);
             $data = str_replace('_', '/', $data);
         }
-        $data=base64_decode($data);
-        $result=self::rc4($data,$rc4key);
-        $result=base64_decode($result);
+        $data = base64_decode($data);
+        $result = $this->rc4($data, $rc4key);
+        $result = base64_decode($result);
         return $result;
     }
 
-    public static function rc4($data, $pwd = "")
+    public function rc4($data, $pwd = "")
     {
-        if(empty($pwd)){
-            $pwd=RC4KEY;
+        if (empty($pwd)) {
+            $pwd = RC4KEY;
         }
         $cipher = "";
         $key[] = "";
